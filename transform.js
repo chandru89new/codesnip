00952b88faf118e1c57c6998b2f905df44c6c783
Chandru <chandru89new@gmail.com>
2020-02-19 16:13:00 +0530

    upgrades transform.js to handle nested objects in definition

--- transform.js
+++ transform.js
@@ -24,12 +24,42 @@ const inputObject = {
   }
 }
 
-const result = extract(definition)(inputObject) 
+const result = transform(definition)(inputObject) 
 {
   name: 'john',
   age: 25,
   email: 'john@doe.com',
   phone: null
+}
+
+Also works with nested objects in definition files. For example:
+===============
+const definition = {
+  name: 'name',
+  meta: {
+    email: 'details.contact.email'
+  }
+}
+
+
+const inputObject = {
+  name: 'john',
+  meta: {
+    age: 25
+  },
+  details: {
+    contact: {
+      email: 'john@doe.com'
+    }
+  }
+}
+
+const result = transform(definition)(inputObject)
+{
+  name: 'john',
+  meta: {
+    email: 'john@doe.com'
+  }
 }
 
 */
@@ -50,7 +80,9 @@ const transform = definition => inputObject => {
   return Object.fromEntries(
     Object.keys(definition).map(key => [
       key,
-      pluck(definition[key])(inputObject)
+      typeof definition[key] === "string"
+        ? pluck(definition[key])(inputObject)
+        : transform(definition[key])(inputObject)
     ])
   );
 };
